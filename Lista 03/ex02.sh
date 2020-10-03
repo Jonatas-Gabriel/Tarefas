7c6e2d4ddba4e4a4e479d8a9997c81f664199cae
Jônatas Gabriel <evangelista.gabriel@academico.ifpb.edu.br>
2020-10-03 13:19:53 -0300

    Lista 02

--- Lista 03/ex02.sh
+++ Lista 03/ex02.sh
@@ -7,15 +7,16 @@ read -p "Digite o nome de mais um arquivo: " arq_2
 read -p "Digite o nome de só mais um arquivo: " arq_3
 
 
-if ((wc $arq_1 > wc $arq_2 && wc $arq_1 > wc $arq_3)); then
-	echo "$1 possui o maior número de linhas"
-	exit 1
-elif ((wc $arq_2 > wc $arq_1 && wc $arq_2 > wc $arq_3)); then
-	echo "$2 possui o maior número de linahs"
-	exit 1
-else
-	echo "$3 possui o maior número de linhas"
-fi 
+wc -l ${arq_1} &> 01 
+wc -l ${arq_2} &> 02
+wc -l ${arq_3} &> 03
+
+01 > 02 && 01 > 03 && echo "${arq_1} tem o maior número de linhas"
+
+02 > 01 && 02 > 03 && echo "${arq_2} tem o maior número de linhas"
+
+03 > 02 && 03 > 01 && echo "${arq_3} tem o maior número de linhas"
+
 
 
 
